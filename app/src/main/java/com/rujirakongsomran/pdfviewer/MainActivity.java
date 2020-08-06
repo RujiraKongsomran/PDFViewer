0188f5053ccb5e2299b542ec2bddc403d4c0e0a8
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-08-06 19:24:36 +0700

    Open PDF from Assets finish

--- app/src/main/java/com/rujirakongsomran/pdfviewer/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/pdfviewer/MainActivity.java
@@ -2,13 +2,28 @@ package com.rujirakongsomran.pdfviewer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnOpenAssets;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnOpenAssets = (Button) findViewById(R.id.btnOpenAssets);
+        btnOpenAssets.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ViewActivity.class);
+                intent.putExtra("ViewType", "assets");
+                startActivity(intent);
+            }
+        });
     }
 }
