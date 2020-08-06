8a455bf8b684eee69a5694e8a84701950e40cfd7
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-08-06 21:18:01 +0700

    Open PDF from device finish

--- app/src/main/java/com/rujirakongsomran/pdfviewer/ViewActivity.java
+++ app/src/main/java/com/rujirakongsomran/pdfviewer/ViewActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.MotionEvent;
@@ -75,6 +76,53 @@ public class ViewActivity extends AppCompatActivity {
                             .enableAnnotationRendering(true)
                             .invalidPageColor(Color.WHITE)
                             .load();
+                } else if (viewType.equals("storage")) {
+                    Uri pdfFile = Uri.parse(getIntent().getStringExtra("FileUri"));
+                    pdfView.fromUri(pdfFile)
+                            .password(null)
+                            .defaultPage(0)
+                            .enableSwipe(true)
+                            .swipeHorizontal(false)
+                            .enableDoubletap(true) // Double tab to zoom
+                            .onDraw(new OnDrawListener() {
+                                @Override
+                                public void onLayerDrawn(Canvas canvas, float pageWidth, float pageHeight, int displayedPage) {
+
+                                }
+                            })
+                            .onDrawAll(new OnDrawListener() {
+                                @Override
+                                public void onLayerDrawn(Canvas canvas, float pageWidth, float pageHeight, int displayedPage) {
+
+                                }
+                            })
+                            .onPageError(new OnPageErrorListener() {
+                                @Override
+                                public void onPageError(int page, Throwable t) {
+                                    Toast.makeText(ViewActivity.this, "Error while open page " + page, Toast.LENGTH_SHORT).show();
+                                }
+                            })
+                            .onPageChange(new OnPageChangeListener() {
+                                @Override
+                                public void onPageChanged(int page, int pageCount) {
+
+                                }
+                            })
+                            .onTap(new OnTapListener() {
+                                @Override
+                                public boolean onTap(MotionEvent e) {
+                                    return true;
+                                }
+                            }).onRender(new OnRenderListener() {
+                        @Override
+                        public void onInitiallyRendered(int nbPages, float pageWidth, float pageHeight) {
+                            pdfView.fitToWidth(); // Fixed screen size
+                        }
+                    })
+                            .enableAnnotationRendering(true)
+                            .invalidPageColor(Color.WHITE)
+                            .load();
+
                 }
             }
         }
